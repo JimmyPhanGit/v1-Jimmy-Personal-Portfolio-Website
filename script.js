30873c64e8cb9b880cd51647f33af8b8fbabb3a8
Jimmy Phan <jimmyphan1@hotmail.com>
2023-03-02 11:11:59 -0500

    updated project section. need to fill with info

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Visit Counter //
 const countEl = document.getElementById("visits");
 
 function updateVisitCount() {
@@ -25,6 +26,7 @@ window.onload = function(){
     updateVisitCount();
 };
 
+// Auto-Type function //
 var typed = new Typed(".auto-type", {
     strings: [
         "Computer Science Student",
@@ -42,3 +44,19 @@ var typed = new Typed(".auto-type", {
     loop: true
 })
 
+// Cards Hover //
+
+const handleOnMouseMove= e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect();
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for(const card of document.querySelectorAll(".card")){
+    card.onmousemove = e => handleOnMouseMove(e);
+}
