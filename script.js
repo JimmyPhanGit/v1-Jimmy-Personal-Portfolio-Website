b1a4447d45e5879c112ba4eba9721cbab8463e61
Jimmy Phan <jimmyphan1@hotmail.com>
2023-11-09 21:28:30 -0500

    Added new changes + Resume

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// Visit Counter //
-const countEl = document.getElementById("visits");
-
-function updateVisitCount() {
-    fetch('https://api.countapi.xyz/update/jimmy-phan/website/?amount=1')
-    .then(res => res.json())
-    .then(res => {
-        countEl.innerHTML = res.value;
-    });
-}
-
+// navbar //
 var navbar = document.getElementById("nav-bar");
 var sticky = navbar.offsetTop;
 
@@ -26,18 +16,46 @@ window.onload = function(){
     updateVisitCount();
 };
 
+// ABOUTMETEXT animation //
+
+    // Function to check if an element is in the viewport
+    function isInViewport(element) {
+        var rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Function to handle scroll events
+    function handleScroll() {
+        var aboutMeTitle = document.querySelector('#ABOUTME h1');
+        var aboutMeTextSection = document.getElementById('ABOUTMETEXT');
+
+        if (isInViewport(aboutMeTitle)) {
+            aboutMeTextSection.classList.add('appear');
+        }
+    }
+
+    // Attach the handleScroll function to the scroll event
+    window.addEventListener('scroll', handleScroll);
+
+    // Initial check in case the section is already in the viewport on page load
+    handleScroll();
+
 // Auto-Type function //
 var typed = new Typed(".auto-type", {
     strings: [
-        "Computer Science Student",
-        "Future Software Developer",
-        "Aspiring Data Analyst",
-        "Fascinated by Integration and Applications",
+        "Software Developer",
+        "Dabbles in Data, UI/UX, Project Management",
         "Cat Dad",
         "Fitness Fanatic",
-        "Trying to better myself",
+        "Always Trying to better myself",
         "Volleyball Enthusiast",
-        "Click the About Me tab above to find out more!"
+        "Marathoner",
+        "Click Hobbies to find out more!"
     ],
     typeSpeed: 100,
     backSpeed: 125,
